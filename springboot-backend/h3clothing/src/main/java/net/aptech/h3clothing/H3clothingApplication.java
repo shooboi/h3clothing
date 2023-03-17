e01c13476107c4682bddd5c2efdbeed80958dc3c
shooboi <82247284+shooboi@users.noreply.github.com>
2023-03-17 10:40:48 +0700

    initalize folder and spring boot project temple

---
+++ springboot-backend/h3clothing/src/main/java/net/aptech/h3clothing/H3clothingApplication.java
@@ -0,0 +1,13 @@
+package net.aptech.h3clothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H3clothingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H3clothingApplication.class, args);
+    }
+
+}
