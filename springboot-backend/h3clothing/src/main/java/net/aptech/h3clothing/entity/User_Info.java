cf8d84ef76f38297eb44d865a01f92fea3ee03ca
SubsectorKirisakyia <108776309+SubsectorKirisakyia@users.noreply.github.com>
2023-03-27 15:28:15 +0700

    Update User_Info.java

--- springboot-backend/h3clothing/src/main/java/net/aptech/h3clothing/entity/User_Info.java
+++ springboot-backend/h3clothing/src/main/java/net/aptech/h3clothing/entity/User_Info.java
@@ -1,14 +1,21 @@
 package net.aptech.h3clothing.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.sql.Date;
 
 @Entity
 @Table(name = "user_info")
 public class User_Info extends Base {
+    @NotBlank
+    @Pattern(regexp = "([A-Za-z]+( [A-Za-z]+)+)")
     @Column(name = "fullname")
     private String fullName;
 
+    @Size(max=10)
+    @Pattern(regexp = "\\d")
     @Column(name = "phone_number")
     private String phoneNumber;
 
